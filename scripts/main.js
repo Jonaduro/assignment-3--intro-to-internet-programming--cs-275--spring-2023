9227fed05bb4f9641a04f0479484297d7414dac7
Jonaduro <durodola@hartford.edu>
2023-04-24 04:30:39 -0400

    modal

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,18 @@ showModalButton.addEventListener('click',showModal);
 function toggleMenuDropdown () {
     menuDropdown.classList.toggle('visible');
 }
+function showModal () {
+ let backgroundLayer = document.createElement('div');
+ backgroundLayer.classList.add('modal-background');
+ modalPanel.appendChild(backgroundLayer);
+
+ let modalContent = document.createElement('div');
+ modalContent.classList.add('modal-content');
+ let heading = document.createElement('h2')
+ heading.textContent ='Modal Title';
+ let paragraph = document.createElement('p')
+ paragraph.textContent = 'Modal content';
+ modalContent.appendChild(header);
+ modalContent.appendChild(paragraph);
+ modalPanel.appendChild(modalContent);
+}
